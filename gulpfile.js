3170d89c27d4a7a52581991b60bddbdf62af2579
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-08-13 11:47:16 +0300

    in the process;)

--- gulpfile.js
+++ gulpfile.js
@@ -195,7 +195,7 @@ export const server = () => {
 
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
-  gulp.watch(path.watch.js, js);
+  //   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.img, image);
   gulp.watch(path.src.fonts, copy);
 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,9 @@
-const headerMenuItem = document.querySelectorAll(".header__nav-item");
+const headerMenuItem = document.querySelectorAll(".header__nav-item-link");
 for (let i = 0; i < headerMenuItem.length; i++) {
   headerMenuItem[i].addEventListener("click", function () {
     for (let y = 0; y < headerMenuItem.length; y++) {
-      headerMenuItem[y].classList.remove("header__menu-item--active");
+      headerMenuItem[y].classList.remove("header__nav-item-link--active");
     }
-    this.classList.add("header__menu-item--active");
+    this.classList.add("header__nav-item-link--active");
   });
 }
-const button = document.querySelector(".button");
-button.addEventListener("click", function () {
-  button.classList.add("button--active");
-});
