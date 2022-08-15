651dba4a8cab3d7921f759a8fdc57092196eb898
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-08-15 17:36:44 +0300

    in the process;)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-//  активация меню элемента
+//  переходы по пунктам меню
 const headerMenuItem = document.querySelectorAll(".header__nav-item-link");
 for (let i = 0; i < headerMenuItem.length; i++) {
   headerMenuItem[i].addEventListener("click", function () {
@@ -8,3 +8,21 @@ for (let i = 0; i < headerMenuItem.length; i++) {
     this.classList.add("header__nav-item-link--active");
   });
 }
+//  ограничения на ввод символов
+ let searchNode = document.getElementById('search')
+ let promptNode = document.getElementById('prompt')
+ const simvolsNode = /['!','@','#','$','%','^','&','*','(',')']/g;
+const promptTextNode = 'Недопустимый символ для ввода'
+ searchNode.oninput = function(){
+  if (this.value.match(simvolsNode)){
+   this.value = this.value.replace(simvolsNode, '') 
+   promptNode.innerHTML = promptTextNode
+}
+else{
+   promptNode.innerHTML = '';
+}
+
+ }
+ 
+
+
