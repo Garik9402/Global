2eda508b35bfec8a55a62d43cd51bba93f72dd66
Garik9402 <gbesati@mail.ru>
2022-09-20 19:04:53 +0300

    in the process;)

--- dist/js/script.js
+++
@@ -1,9 +0,0 @@
-const headerMenuItem = document.querySelectorAll(".header__nav-item-link");
-for (let i = 0; i < headerMenuItem.length; i++) {
-  headerMenuItem[i].addEventListener("click", function () {
-    for (let y = 0; y < headerMenuItem.length; y++) {
-      headerMenuItem[y].classList.remove("header__nav-item-link--active");
-    }
-    this.classList.add("header__nav-item-link--active");
-  });
-}
